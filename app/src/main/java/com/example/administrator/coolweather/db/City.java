c676183763fcee6183efcd192b488f1ed5677e7f
wbf <bingpkk123@qq.com>
2017-10-11 22:34:41 +0800

    加入创建数据库

---
+++ app/src/main/java/com/example/administrator/coolweather/db/City.java
@@ -0,0 +1,17 @@
+package com.example.administrator.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2017/10/11.
+ */
+
+public class City extends DataSupport {
+    private int id;
+
+    private String cityName;
+
+    private int cityCode;
+
+
+}
